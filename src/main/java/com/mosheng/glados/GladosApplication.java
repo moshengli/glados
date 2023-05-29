5c443167cd53fb09944371d6eb3c1c3c3375970f
李晨亮 <1031809056@qq.com>
2023-05-29 09:21:46 +0000

    0.0.1
    Signed-off-by: 李晨亮 <1031809056@qq.com>

---
+++ src/main/java/com/mosheng/glados/GladosApplication.java
@@ -0,0 +1,13 @@
+package com.mosheng.glados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GladosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GladosApplication.class, args);
+    }
+
+}
